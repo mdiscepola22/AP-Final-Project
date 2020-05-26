5c53c566c8b18a1901d6451568ad674f07374dd1
mdisc <mdisc@DESKTOP-HQF5PEE.hsd1.pa.comcast.net>
2020-05-26 16:07:46 -0400

    me

--- Frame.java
+++ Frame.java
@@ -1,8 +1,20 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Frame {
+public class Frame extends JFrame{
+	
+	public Frame(){
+		setResizable(false);
+		setSize(1920,1080);
+		setLocation(0,0);
+		setTitle("Juice Tycoon");
+		
+		JPanel menu = new JPanel();
+		menu.setSize(width, 1080);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new Frame();
 
 	}
 

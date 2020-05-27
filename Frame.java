d35b342f70e8d5e2a685e86788fdae51eed70634
mdisc <mdisc@DESKTOP-HQF5PEE.hsd1.pa.comcast.net>
2020-05-27 18:39:07 -0400

    drngv isukhlsl

--- Frame.java
+++ Frame.java
@@ -1,10 +1,13 @@
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javax.swing.ComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -66,14 +69,14 @@ public class Frame extends JFrame{
 		menu.add(jps);
 		
 		final JLabel rpm = new JLabel("rpm");
-		rpm.setSize(360, 44);
+		rpm.setSize(360, 46);
 		rpm.setFont(new Font("Courier", Font.PLAIN, 40));
 		rpm.setLocation(20, 280);
 		rpm.setVisible(true);
 		menu.add(rpm);
 		
 		
-		JButton buyFac = new JButton("Buy a Factory");
+		JButton buyFac = new JButton("Buy a Factory for $2000");
 		buyFac.setSize(360, 60);
 		buyFac.setLocation(20, 360);
 		buyFac.addActionListener(new ActionListener() {
@@ -85,7 +88,7 @@ public class Frame extends JFrame{
 		menu.add(buyFac);
 		
 		
-		JButton buyOrc = new JButton("Buy an Orchard");
+		JButton buyOrc = new JButton("Buy an Orchard for $2000");
 		buyOrc.setSize(360, 60);
 		buyOrc.setLocation(20, 450);
 		buyOrc.addActionListener(new ActionListener() {
@@ -95,6 +98,42 @@ public class Frame extends JFrame{
 			}
 		});
 		menu.add(buyOrc);
+		
+		
+		final JComboBox<String> facUpgrade = new JComboBox<String>();
+		facUpgrade.setSize(118, 40);
+		facUpgrade.setLocation(20, 560);
+		facUpgrade.addItem("Choose Factory");
+		menu.add(facUpgrade);
+		
+		JButton upgradeFac = new JButton("Upgrade Factory");
+		upgradeFac.setSize(230, 60);
+		upgradeFac.setLocation(150, 550);
+		upgradeFac.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				stats.factoryUpgrade(Integer.parseInt((String) facUpgrade.getSelectedItem()));
+			}
+		});
+		menu.add(upgradeFac);
+		
+		
+		final JComboBox<String> orcUpgrade = new JComboBox<String>();
+		orcUpgrade.setSize(118, 40);
+		orcUpgrade.setLocation(20, 650);
+		orcUpgrade.addItem("Choose Orchard");
+		menu.add(orcUpgrade);
+		
+		JButton upgradeOrc = new JButton("Upgrade Orchard");
+		upgradeOrc.setSize(230, 60);
+		upgradeOrc.setLocation(150, 640);
+		upgradeOrc.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				stats.orchardUpgrade(Integer.parseInt((String) orcUpgrade.getSelectedItem()));
+			}
+		});
+		menu.add(upgradeOrc);
 		
 		
 		
@@ -109,6 +148,17 @@ public class Frame extends JFrame{
 				lps.setText(stats.getLPS() + " lemons/s");
 				jps.setText(stats.getJPS() + " juice/s");
 				rpm.setText(stats.getRPM() + " research/m");
+				
+				
+				if(facUpgrade.getItemCount() < stats.getNumFactories() + 1)
+				{
+					facUpgrade.addItem("" + stats.getNumFactories());
+				}
+				
+				if(orcUpgrade.getItemCount() < stats.getNumOrchards() + 1)
+				{
+					orcUpgrade.addItem("" + stats.getNumOrchards());
+				}
 
 			}
 		}, 0, 100);
